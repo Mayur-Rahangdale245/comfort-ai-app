7f0a2c4b9f5ccb2402f36c8d7fb5d74222fda9cf
Mayur-Rahangdale245 <mayurahangdale145@gmail.com>
2026-03-15 15:18:47 +0530

    fix backend endpoint

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ let message=document.getElementById("userInput").value
 
 document.getElementById("chatBox").innerText="Soch raha hoon..."
 
-const response=await fetch("https://comfort-ai-backend.onrender.com",{
+const response = await fetch("https://comfort-ai-backend.onrender.com/chat",{
 method:"POST",
 headers:{
 "Content-Type":"application/json"
 },
 body:JSON.stringify({message})
 })
-
 const data=await response.json()
 
 document.getElementById("chatBox").innerText=data.reply
